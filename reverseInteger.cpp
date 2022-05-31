adfe9a1effe64198759dad4387289f01cad4877c
TheRAWagent <77039476+TheRAWagent@users.noreply.github.com>
2022-05-31 12:52:59 +0530

    .

---
+++ reverseInteger.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long temp=0;
+while(x){
+temp=temp*10 + x%10;
+x/=10;
+}
+if(temp>INT_MAX || temp<INT_MIN)
+return 0;
+return temp;
+
+    }
+};
